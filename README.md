a89f3aaa65a3c88ce0a7861f8cb080ce3f00165e
adeagbotoheeb <81494027+adeagbotoheeb@users.noreply.github.com>
2022-10-12 23:53:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-sgcufs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-sgcufs)
---
+++ src/components/button.js
@@ -0,0 +1,14 @@
+import "./button.css";
+import React from "react"
+
+const Button = ({ symbol, color, handleClick }) => {
+  return (
+    <div 
+    onClick={() => handleClick(symbol)}
+    className="button-wrapper" style={{ backgroundColor: color }}>
+      {symbol}
+    </div>
+  );
+};
+
+export default Button;
